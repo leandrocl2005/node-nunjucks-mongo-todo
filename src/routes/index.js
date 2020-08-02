2d851ca45c5189eab595d0c9e19a7649d2ea1b3a
leandrocl2005 <leandrocl2005@yahoo.com.br>
2020-08-02 17:32:56 -0300

    include oscar project

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ const router = express.Router()
 const HomeController = require("../controllers/HomeController")
 router.get("/", HomeController.index)
 
+/* ===== Oscars trailers Route ===== */
+router.get("/oscars", (_req, res) => res.render("oscars.njk"))
+
 /* ===== Todos Route ===== */
 const todos = require("./todos")
 router.use("/todos", todos)
